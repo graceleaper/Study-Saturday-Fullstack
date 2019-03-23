cbed46355ae5e82be7066a72721a32ede9a7d823
Grace Lee <igracelee1@gmail.com>
2019-03-23 12:20:50 -0400

    cycle-1

--- browser/components/Main.js
+++ browser/components/Main.js
@@ -10,9 +10,11 @@ export default class Main extends Component {
     this.state = {
       students: [],
       selectedStudent: {},
+      showStudent: false
     };
 
     this.selectStudent = this.selectStudent.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -35,10 +37,33 @@ export default class Main extends Component {
     });
   }
 
+  //what we'll be putting in our new student form
+  async addStudent(student) {
+    const { data } = await axios.post('/student', student);
+    this.setState({
+      students: [...this.state.students, data],
+      showStudent: false,
+    });
+  }
+
+  handleClick(event) {
+    this.setState({
+      //when our 'Add New Student' button is clicked, 
+      //our students will show up
+      showStudent: !this.state.showStudent
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Students</h1>
+        <button onClick={this.handleClick}>Add New Student</button>
+        {/*if our state for showStudent is true (which happens
+        AFTER we click our button, then the form below will show up) */}
+        {this.state.showStudent ? (
+          <NewStudentForm addStudent={this.addStudent} />
+        ) : null} {/*if this.state.showStudent is false, DON'T show the form*/}
         <table>
           <thead>
             <tr>
