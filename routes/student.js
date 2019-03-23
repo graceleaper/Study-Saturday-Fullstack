572ace39921a4341247750304a72c8b4b505c959
Grace Lee <igracelee1@gmail.com>
2019-03-23 10:21:26 -0400

    cycle-0

--- routes/student.js
+++ routes/student.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const Student = require('../db/models/students');
-const Test = require('../db/models/tests');
+const router = require("express").Router();
+const Student = require("../db/models/students");
+const Test = require("../db/models/tests");
 
-router.get('/:studentId', function(req, res, next) {
+router.get("/:studentId", function(req, res, next) {
   Student.findById(req.params.studentId)
     .then(student => {
       if (!student) return res.sendStatus(404);
@@ -11,28 +11,37 @@ router.get('/:studentId', function(req, res, next) {
     .catch(next);
 });
 
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   Student.findAll({ include: { all: true } }).then(students =>
     res.json(students)
   );
 });
 
-router.put('/:id', function(req, res, next) {
+router.put("/:id", function(req, res, next) {
   Student.update(req.body, {
     where: {
-      id: req.params.id,
+      id: req.params.id
     },
-    returning: true,
+    returning: true
   })
     .then(test => res.status(201).json(test[1][0]))
     .catch(next);
 });
 
-router.delete('/:id', function(req, res, next) {
+router.post("/", async (req, res, next) => {
+  try {
+    const newStudent = await Student.create(req.body);
+    res.json(newStudent);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", function(req, res, next) {
   Student.destroy({
     where: {
-      id: req.params.id,
-    },
+      id: req.params.id
+    }
   })
     .then(() => {
       res.sendStatus(204);
